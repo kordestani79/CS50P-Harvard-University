c1f6279f3d96dbbad1c76dcba5a0eb955ce0b002
Maryam Kordestani <76499677+kordestani79@users.noreply.github.com>
2024-06-21 16:13:56 +0330

    Delete Week0/faces.py

--- Week0/faces.py
+++
@@ -1,8 +0,0 @@
-def convert_emoticons(text):
-    text = text.replace(":)", "🙂")
-    text = text.replace(":(", "🙁")
-    return text
-
-user_input = input()
-converted_text = convert_emoticons(user_input)
-print(converted_text)
